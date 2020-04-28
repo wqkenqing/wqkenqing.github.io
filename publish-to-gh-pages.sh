9d5cb9d9c333890156f19d4982b88a800b6f2d96
kuiq.wang <wqkenqingto@163.com>
2020-04-28 17:10:02 +0800

    update

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -19,7 +19,8 @@ git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
 
 # Github Pages
-git push --force --quiet "https://github.com/wqkenqing/wqkenqing.github.io.git" master:master
+git push --force --quiet "https://${REPO_TOKEN}@${GH_REF}" master:master
+
 
 # Coding Pages
 #git push --force --quiet "https://Leafney:${CodingToken}@${CD_REF}" master:master
